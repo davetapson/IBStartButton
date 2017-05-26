e80adf4054f004e81094046df14cd83701396446
David Tapson <dave.tapson@gmail.com>
2017-05-26 23:20:16 +0200

    Start Button Project

---
+++ WindowsFormsApplication1/StartForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartForm));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.chkKeepRunning = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Green;
+            this.btnStart.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnStart.Location = new System.Drawing.Point(80, 40);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(124, 105);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStatus.Location = new System.Drawing.Point(80, 170);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(124, 20);
+            this.txtStatus.TabIndex = 1;
+            this.txtStatus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // chkKeepRunning
+            // 
+            this.chkKeepRunning.AutoSize = true;
+            this.chkKeepRunning.Location = new System.Drawing.Point(12, 214);
+            this.chkKeepRunning.Name = "chkKeepRunning";
+            this.chkKeepRunning.Size = new System.Drawing.Size(94, 17);
+            this.chkKeepRunning.TabIndex = 2;
+            this.chkKeepRunning.Text = "Keep Running";
+            this.chkKeepRunning.UseVisualStyleBackColor = true;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 243);
+            this.Controls.Add(this.chkKeepRunning);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IB Starter Button";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.CheckBox chkKeepRunning;
+    }
+}
+
---
+++ WindowsFormsApplication1/StartForm.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Start();
+
+            //#/bin/sh
+            //# Run the IB Gateway by passing along all of the necessary parameters  
+            //# Syntax is:
+            //# java -cp [java options] [IBJts directory] [username=XXX] [password=ZZZ] 
+            //# Get the username from command-line parameter 1 (or hard code this USER=xxxxx)
+            //            USER =$1
+            //# Get password from command-line parameter 2 (or hard code this PASS=xxxxx)
+            //PASS =$2
+            //# Get the current date and time (to the minute)
+            //DATE =`date +% Y % m % d % H % M`
+            //# Java Executable - be specific about this so you know which JRE you are using
+            //JAVAEXE =/ usr / bin / java
+            //# Java Runtime Options - these specify which jts.jar files to work with and how to
+            //# configure Java's memory model
+            //JAVAOPTIONS = "-cp jts.jar:total.2013.jar -Dsun.java2d.noddraw=true -Xmx512M ibgateway.GWClient"
+            //# Log file - make sure whatever you name this file has enough space in the folder
+            //LOGFILE =/ tmp / ibg_running_${ DATE}.log
+            //# IBJts working directory - change this to where you have the IB Gateway installed
+            //            IBJTSDIR =/ home / rholowczak / ib / IBJts
+            //# Assemble the command line and run it! 
+            //${ JAVAEXE} ${ JAVAOPTIONS} ${ IBJTSDIR}
+            //            username =${ USER}
+            //            password =${ PASS} > ${ LOGFILE}
+            //            2 > &1
+            //# Note the  2>&1  at the end of the command line re-directs standard error into the same 
+            //# log file as standard outpu
+        }
+
+        private static void Start()
+        {
+            string userName = "d4v3t4ps1";
+            string password = "Makatini1";
+            DateTime date = DateTime.Now;
+            string javaPath = @"C:\Program Files (x86)\Java\jdk1.8.0_92\jre\bin\java";
+            string javaOptions = @" -cp jts.jar:total.2013.jar - Dsun.java2d.noddraw = true - Xmx512M ibgateway.GWClient ";
+            string logFile = @" C:\temp\ib_gatewayibg_running_" + date + ".log";
+            string IBJTSDIR = @" C:\Jts\ibgateway\952\jars";
+
+            string script = javaPath + javaOptions + IBJTSDIR + " username = " + userName + " password = " + password + " > " + logFile + " 2>&1";
+
+            try
+            {
+                var proc6 = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = @"C:\Jts\ibgateway\952\ibgateway.exe",
+                        Arguments = " username=" + userName + " password=" + password,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = false,
+                        WorkingDirectory = @"C:\Jts\ibgateway\952\"
+                    }
+                };
+
+                proc6.Start();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DoTick();
+        }
+
+        private void DoTick()
+        {
+            Process[] pname = Process.GetProcessesByName("ibgateway");
+            if (pname.Length == 0)
+            {
+                txtStatus.Text = "Not running :(";
+                txtStatus.BackColor = Color.Gray;
+                btnStart.Enabled = true;
+                btnStart.BackColor = Color.Green;
+
+                if (chkKeepRunning.Checked) Start();
+            }
+            else
+            {
+                txtStatus.Text = "Running :)";
+                txtStatus.BackColor = Color.Green;
+                btnStart.BackColor = Color.Gray;
+                btnStart.Enabled = false;
+            }
+        }
+    }
+}
